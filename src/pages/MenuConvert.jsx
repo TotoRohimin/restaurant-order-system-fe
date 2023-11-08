6b681e398ca7ea976056785109ae340b75c6132a
toomiinn <butouchiha9@gmail.com>
2023-11-08 22:00:39 +0700

    Create MenuConvert.jsx

---
+++ src/pages/MenuConvert.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TextMenu from "../components/atoms/Text";
+import ButtonMenu from "../components/atoms/Button";
+
+function MenuConvert() {
+  return (
+    <div>
+      <TextMenu />
+      <ButtonMenu />
+    </div>
+  );
+}
+
+export default MenuConvert;
