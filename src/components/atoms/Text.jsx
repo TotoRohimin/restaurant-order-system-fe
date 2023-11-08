13a6e78fa6f63f7f06c547c5e7b2f3b4ea949db0
toomiinn <butouchiha9@gmail.com>
2023-11-08 15:04:28 +0700

    (ConvertMenuPage/feat): Membuat Button dan Text

---
+++ src/components/atoms/Text.jsx
@@ -0,0 +1,11 @@
+export default function Text() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Convert Menu</h1>
+      <h3 className="text-xl font-semibold mt-4">Menambah menu file CSV</h3>
+      <p className="mt-2">
+        Sesuaikan data dengan format yang telah ditentukan. Download Template terlebih dahulu, isi data <br /> yang sesuai kemudian simpan dalam bentuk csv dan unggah pada laman dibawah
+      </p>
+    </div>
+  );
+}
