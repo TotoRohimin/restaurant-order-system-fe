08111ebd81b7755bd5132b3f09de740ecd5c6eb2
toomiinn <butouchiha9@gmail.com>
2023-11-08 22:00:52 +0700

    update index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,43 +1,48 @@
-import { Route, Routes } from 'react-router-dom';
-import AppShell from '../components/templates/AppShell';
-import Dashboard from '../pages/Dashboard';
+import { Route, Routes } from "react-router-dom";
+import AppShell from "../components/templates/AppShell";
+import Dashboard from "../pages/Dashboard";
+import MenuConvert from "../pages/MenuConvert";
 
 export default function root() {
   const dataRoute = [
     {
-      path: '/',
+      path: "/",
       element: <h1>Landing Page</h1>,
-      middleware: 'guest',
+      middleware: "guest",
     },
     {
-      path: '/login',
+      path: "/login",
       element: <h1>Login</h1>,
-      middleware: 'guest',
+      middleware: "guest",
     },
     {
-      path: '/admin',
+      path: "/admin",
       element: <AppShell />,
-      middleware: 'guest',
+      middleware: "guest",
       withChildren: [
         {
-          path: '/',
+          path: "/",
           element: <Dashboard />,
         },
         {
-          path: '/analisis',
+          path: "/analisis",
           element: <h1>Analisis</h1>,
         },
         {
-          path: '/pesanan',
+          path: "/pesanan",
           element: <h1>Pesanan</h1>,
         },
         {
-          path: '/menu',
+          path: "/menu",
           element: <h1>Menu</h1>,
         },
         {
-          path: '/rating',
+          path: "/rating",
           element: <h1>Rating</h1>,
+        },
+        {
+          path: "/admin/menuconvert",
+          element: <MenuConvert />,
         },
       ],
     },
@@ -46,20 +51,9 @@ export default function root() {
   return (
     <Routes>
       {dataRoute.map((route, index) => (
-        <Route
-          key={index}
-          path={route.path}
-          element={
-            route.middleware === 'guest' ? route.element : route.element
-          }>
+        <Route key={index} path={route.path} element={route.middleware === "guest" ? route.element : route.element}>
           {route.withChildren?.map((outlet, index) => (
-            <Route
-              key={index}
-              path={route.path + outlet.path}
-              element={
-                outlet.middleware === 'guest' ? outlet.element : outlet.element
-              }
-            />
+            <Route key={index} path={route.path + outlet.path} element={outlet.middleware === "guest" ? outlet.element : outlet.element} />
           ))}
         </Route>
       ))}
