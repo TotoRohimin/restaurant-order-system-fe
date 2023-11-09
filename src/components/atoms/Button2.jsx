beb8e9fff034e0dbe3282ed27beb939b38335519
toomiinn <butouchiha9@gmail.com>
2023-11-09 22:40:43 +0700

    (ConvertMneuPage/feat): Create Button2.jsx

---
+++ src/components/atoms/Button2.jsx
@@ -0,0 +1,7 @@
+export default function ButtonMenu2() {
+  return (
+    <div>
+      <button className="bg-orange-900 hover:bg-orange-950 text-white font-bold py-4 px-14 rounded-full mb-4 mt-4">Unggah File</button>;
+    </div>
+  );
+}
