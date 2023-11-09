ebfc74acd28733e7d4e1cf68de8328dff3e9d4d5
toomiinn <butouchiha9@gmail.com>
2023-11-09 22:39:18 +0700

    (ConvertMneuPage/feat): Update MenuConvert.jsx

--- src/pages/MenuConvert.jsx
+++ src/pages/MenuConvert.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import TextMenu from "../components/atoms/Text";
 import ButtonMenu from "../components/atoms/Button";
+import UploadFile from "../components/molecules/Form";
+import ButtonMenu2 from "../components/atoms/Button2";
 
 function MenuConvert() {
   return (
-    <div>
+    <div className="bg-gray-100 p-8 rounded-3xl shadow-md max-w-full mx-auto">
       <TextMenu />
       <ButtonMenu />
+      <UploadFile />
+      <ButtonMenu2 />
     </div>
   );
 }
