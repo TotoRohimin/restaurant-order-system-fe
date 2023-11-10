f36cb1288b0697ed676662cc914b30a771f2fe6c
toomiinn <butouchiha9@gmail.com>
2023-11-10 23:01:19 +0700

    (CovertMenuPage/feat): update Text.jsx

--- src/components/atoms/Text.jsx
+++ src/components/atoms/Text.jsx
@@ -1,9 +1,9 @@
 export default function TextMenu() {
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-14">Convert Menu</h1>
-      <h3 className="text-2xl font-semibold mb-14">Menambah menu file CSV</h3>
-      <p className="mb-14 text-lg">
+      <h1 className="text-4xl font-bold mb-8">Convert Menu</h1>
+      <h3 className="text-2xl font-semibold mb-8">Menambah menu file CSV</h3>
+      <p className="mb-8 text-lg">
         Sesuaikan data dengan format yang telah ditentukan. Download Template terlebih dahulu, isi data <br /> yang sesuai kemudian simpan dalam bentuk csv dan unggah pada laman dibawah
       </p>
     </div>
