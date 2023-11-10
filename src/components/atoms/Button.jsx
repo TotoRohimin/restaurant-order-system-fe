a44a21c66d138220827fe6b7b567f3bbaa142e7c
toomiinn <butouchiha9@gmail.com>
2023-11-10 23:00:59 +0700

    (CovertMenuPage/feat): update Button.jsx

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,7 @@
 export default function ButtonMenu() {
   return (
     <div>
-      <button className="bg-orange-900 hover:bg-orange-950 text-white font-bold py-4 px-14 rounded-full mb-8">Download Template</button>;
+      <button className="bg-orange-900 hover:bg-orange-950 text-white font-bold py-4 px-16 rounded-full mb-8">Download Template</button>;
       <div>
         <h3 className="text-2xl font-semibold mb-8">Unggah menu file CSV</h3>
       </div>
